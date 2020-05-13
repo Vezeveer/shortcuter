71dc923d527638dd76191246ea1e13c9b9e4c8f0
vezeveer <mortalmanny@live.com>
2020-05-13 10:36:29 +0800

    Add project files.

---
+++ shortcuter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace shortcuter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Process.Start(args[0]);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($@"Program not found at location: {args[0]}");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
